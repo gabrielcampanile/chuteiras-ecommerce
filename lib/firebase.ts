05f7ce295f63765bcdf91c4d2b5d7ad73d6afae4
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2025-06-30 11:38:30 -0300

    feat: setup Firebase configuration

---
+++ lib/firebase.ts
@@ -0,0 +1,32 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCSySCoKjZ9Be8Ckrg_zGgOCBk4p6vYl8",
+  authDomain: "chuteiras-pro.firebaseapp.com",
+  projectId: "chuteiras-pro",
+  storageBucket: "chuteiras-pro.firebasestorage.app",
+  messagingSenderId: "752475725101",
+  appId: "1:752475725101:web:93296a8fba1a4acee587a4"
+};
+
+// Initialize Firebase
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+// Initialize Firebase services
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export default app; 
